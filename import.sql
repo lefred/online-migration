8b4929853ac1d22d01f8729cf64f210dce2e1b96
Frederic -lefred- Descamps <lefred@percona.com>
2013-04-19 15:08:48 +0200

    removing files
    Signed-off-by: Frederic -lefred- Descamps <lefred@percona.com>

--- import.sql
+++
@@ -1,8 +0,0 @@
-alter table t 
-add ( 
-nationality varchar(20)
-);
-create table countries
-( code char(2) primary key, name varchar (30));
-
-insert into countries values ('BE','BELGIUM'),('IT','ITALY');
